336bcc4c30935111e5896ecddf1962aaede7dc42
XtremeTHN <nigthmaresans2@gmail.com>
2023-12-27 15:50:09 -0600

    Database population working, Gmail class retrieves all emails and stores correctly into the database

--- src/modules/gmail.py
+++ src/modules/gmail.py
@@ -3,12 +3,18 @@ import os
 import sys
 import pickle
 import sqlite3
-import base64
+
+import keyring
+from cryptography.fernet import Fernet
+
+if keyring.get_password("com.github.Axel.MailManager", "axel") is None:
+    keyring.set_password("com.github.Axel.MailManager", "axel", Fernet.generate_key().decode("utf-8"))
 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+from datetime import datetime
 from gi.repository import GObject
 
 # GObject signals are defined with the __gsignals__ property
@@ -38,41 +44,174 @@ print(DATABASE_PATH)
 class GmailDatabase:
     def __init__(self, db_path: str):
         self.path = db_path
-        self.database = sqlite3.connect(db_path)
+
+        self._decrypt()
+        self.database = sqlite3.connect(db_path, check_same_thread=False)
         self.cursor = self.database.cursor()
         
         self.cursor.execute("BEGIN TRANSACTION;")
         self.cursor.execute("""
 CREATE TABLE IF NOT EXISTS "Email" (
-	"ID"	INTEGER,
-	"SenderName"	TEXT NOT NULL,
-	"SenderIcon"	TEXT,
-	"SenderEmail"	TEXT NOT NULL,
-	"RecieverName"	TEXT NOT NULL,
-	"RecieverEmail"	TEXT NOT NULL,
-	"Subject"	TEXT,
-	"Body"	TEXT NOT NULL,
-	PRIMARY KEY("ID" AUTOINCREMENT)
+    "ID"	INTEGER,
+    "MSGID"	TEXT NOT NULL,
+    "SenderName"	TEXT NOT NULL,
+    "SenderIcon"	TEXT,
+    "SenderEmail"	TEXT NOT NULL,
+    "RecieverName"	TEXT NOT NULL,
+    "RecieverEmail"	TEXT NOT NULL,
+    "Date"	TEXT NOT NULL,
+    "Subject"	TEXT,
+    "Body"	TEXT NOT NULL,
+    PRIMARY KEY("ID" AUTOINCREMENT)
 );
 """)
         self.cursor.execute("COMMIT;")
-    def get_emails(self):
+    
+    def _decrypt(self):
+        """
+        Decrypts the database if it is encrypted
+        This should not be called manually, __init__ calls it automatically
+
+        Parameters:
+            self (GmailDatabase): The current instance of the class.
+        
+        Returns:
+            None
+        """
+        if os.path.exists(DATABASE_PATH + '.enc') is True:
+            with open(DATABASE_PATH + '.enc', 'rb') as file:
+                data = file.read()
+            f = Fernet(keyring.get_password("com.github.Axel.MailManager", "axel"))
+            decrypted = f.decrypt(data)
+            with open(DATABASE_PATH, 'wb') as file:
+                file.write(decrypted)
+            print(DATABASE_PATH + '.enc')
+            os.remove(DATABASE_PATH + '.enc')
+
+    def _encrypt(self):
+        """
+        Encrypts the database if it is not encrypted
+        This should not be called manually, a signal will call this method when the app is closed
+
+        Parameters:
+            self (GmailDatabase): The current instance of the class.
+        
+        Returns:
+            None
+        """
+        with open(DATABASE_PATH, 'rb') as file:
+            data = file.read()
+        f = Fernet(keyring.get_password("com.github.Axel.MailManager", "axel"))
+        encrypted = f.encrypt(data)
+        with open(DATABASE_PATH + '.enc', 'wb') as file:
+            file.write(encrypted)
+        os.remove(DATABASE_PATH)
+
+    def get_emails(self) -> list[tuple]:
+        """
+        Retrieves all emails from the database.
+
+        Parameters:
+            self (GmailDatabase): The current instance of the class.
+
+        Returns:
+            list[tuple]: A list of all emails retrieved from the database.
+        """
         return self.cursor.fetchall()
         
-    def get_email(self, id):
-        self.cursor.execute(f"SELECT * FROM Email WHERE ID = {id};")
-        return self.cursor.fetchall()
+    def get_email(self, id) -> list[tuple]:
+        """
+        Get the email from the database based on the given ID.
+
+        Parameters:
+            id (int): The ID of the email to retrieve. If -1, retrieve the latest email.
+
+        Returns:
+            list[tuple]: A list of email(s) retrieved from the database.
+        """
+        if id == -1:
+            self.cursor.execute(f"SELECT * FROM Email ORDER BY ID DESC LIMIT 1;")
+            return self.cursor.fetchall()
+        else:
+            self.cursor.execute(f"SELECT * FROM Email WHERE ID = {id};")
+            return self.cursor.fetchall()
     
     def get_last_email(self):
+        """
+        Retrieves the last email from the database
+
+        Returns:
+            None.
+        """
         self.cursor.execute("SELECT * FROM Email ORDER BY column DESC LIMIT 1;")
     
-    def add_email(self, body, sender_name, sender_icon, sender_email, reciever_name, reciever_email, subject=""):
+    def add_email(self, 
+                  msg_id, 
+                  sender_name, 
+                  sender_icon, 
+                  sender_email, 
+                  reciever_name, 
+                  reciever_email, 
+                  date, body, subject=""):
+        """
+        Inserts an email into the Email table in the database.
+
+        Parameters:
+            msg_id (str): The ID of the email message.
+            sender_name (str): The name of the sender.
+            sender_icon (str): The icon of the sender.
+            sender_email (str): The email address of the sender.
+            receiver_name (str): The name of the receiver.
+            receiver_email (str): The email address of the receiver.
+            date (str): The date the email was sent.
+            body (str): The body of the email.
+            subject (str, optional): The subject of the email. Defaults to "".
+
+        Returns:
+            None
+        """
+
+        
         self.cursor.execute(f"""
-INSERT INTO Email VALUES(NULL, "{sender_name}","{sender_icon}","{sender_name}","{sender_icon}","{sender_email}","{reciever_name}","{reciever_email}");
+INSERT INTO Email VALUES(NULL, "{msg_id}", "{sender_name.replace('"', '')}","{sender_icon}","{sender_email}","{reciever_name.replace('"', '')}","{reciever_email}", "{date}", "{subject.replace('"', '')}","{body}");
 """)
+
+    def is_empty(self):
+        """
+        Checks if the database is empty by executing a SQL query and returning
+        True if the count of rows is equal to 0.
+
+        Parameters:
+            self (object): The object instance.
+
+        Returns:
+            bool: True if the table is empty, False otherwise.
+        """
+        return self.cursor.execute("SELECT count(*) FROM Email;").fetchone()[0] == 0
+
+    def save(self):
+        """
+        Saves current changes to the database
+
+        Parameters:
+            self (object): The object instance.
+        
+        Returns:
+            None
+        """
         self.database.commit()
-        print("added email")
     
+    def close(self):
+        """
+        Closes the database
+
+        Parameters:
+            self (object): The object instance.
+        
+        Returns:
+            None
+        """
+        self.database.close()
         
 class Gmail(GObject.GObject, threading.Thread):
     __gsignals__ = {
@@ -111,10 +250,8 @@ class Gmail(GObject.GObject, threading.Thread):
 
         self.gmail = None
         self.oauth = None
-        self.database: sqlite3.Connection = None
+        self.database: GmailDatabase = None
 
-        self.start()
-    
     def _add_to_prog(self, msg):
         self.set_property("stack", str(msg))
 
@@ -156,78 +293,133 @@ class Gmail(GObject.GObject, threading.Thread):
         self.gmail = build('gmail', 'v1', credentials=creds)
 
         self._add_to_prog("Authentication finished")
-        self.emit('authentication-finish')
+        self.emit('authentication-finish')    
 
-        self.people = build('people', "v1", credentials=creds)
-    
-    # def emails(self):
-    #     res = self.gmail.users().messages().list(userId='me').execute()
-    #     messages = res.get('messages')
-
-    #     for msg in messages:
-    #         txt = self.gmail.users().messages().get(userId='me', id=msg['id']).execute()
-    #         print(txt)
-    
-    def _get_account_photo(self, sender, reciever) -> list[str]:
-        sender = sender.split("<")[1].replace(">", "").split("@")[0]
-        reciever = reciever.split("<")[1].replace(">", "").split("@")[0]
-        self.people.people().connections().list(
-            resourceName=f"people/{sender}",
-            
-        )
-
-    def _database_item(self, rq_id, res: dict, exc):
-        try:
-            payload=res['payload']
-            headers=payload['headers']
-
-            for d in headers:
-                if d['name'] == 'Subject':
-                    subject = d['value']
-                if d['name'] == 'From':
-                    sender = d['value']
-                if d['name'] == 'To':
-                    reciever = d['value']
-            
-            parts = payload.get('parts')[0]
-            data = parts['body']['data'].replace("-","+").replace("_","/")
-
-            print("adding to database", sender)
-            self.database.add_email(data, sender, "user-symbolic", "example@gmail.com", "axel", "example", subject=subject)
-            print(self._get_account_photo(sender, reciever))
-        except Exception as e:
-            print("Exception: ",e)
-    
-    def start_database(self):
+    def parse_email(self, res: dict):
         """
-        Initializes the database for the Gmail object.
-        Use this method if you need to call the update_database method in another thread
+        Parses an email response and extracts relevant information to store in the database.
+
+        Parameters:
+            res (dict): The email response dictionary.
 
         Returns:
             None
         """
+        try:
+            payload=res['payload']
+            headers=payload['headers']
+
+            id = res['id']
+            created_time=res['internalDate']
+            for d in headers:
+                if d['name'] == 'Subject':
+                    subject = d['value']
+                if d['name'] == 'From':
+                    # Sender name comes in this format "Name <email@server.com>". So we just split the string with this character "<".
+                    sender_data: list[str] = d['value'].split('<')
+
+                    # sender[0] = Name and sender[1] = email@server.com
+
+                    sender = [sender_data[0], sender_data[1].split('>')[0]]
+                if d['name'] == 'To':
+                    # Same as above
+                    try:
+                        reciever_data: list[str] = d['value'].split('<')
+                        reciever = [reciever_data[0], reciever_data[1].split('>')[0]]
+                    except:
+                        reciever = [d['value'], d['value']]
+            parts = list(filter(lambda x: x['mimeType'] == 'text/html', payload.get('parts')))[0]
+
+            if parts is not None:
+                data = parts['body']['data'].replace("-","+").replace("_","/")
+            else:
+                parts = payload.get('parts')[0]
+                data = parts['body']['data'].replace("-","+").replace("_","/")
+
+            self.database.add_email(id, data, sender[0], "avatar-default", sender[1], reciever[0], reciever[1], created_time, subject=subject)
+
+        except Exception as e:
+            print(f"{type(e).__name__}: {e}")
+    
+    def start_database(self):
+        """
+        Initializes the database for the Gmail object.
+        Use this method if you need to manipulate the database
+
+        Returns:
+            GmailDatabase: The database object
+        """
         if self.database is not None:
-            self.database.close()
+            self.database.database.close()
         self.database = GmailDatabase(DATABASE_PATH)
 
-    def update_database(self):
+        return self.database
+
+    def get_chunk_of_emails(self, chunk: int, nextPageToken:str=None, **parameters):
         """
-            Updates/Set's emails info into the database
-            Use this method in the same thread that you called start_database method
-            If Gmail.database is None, it will initialize the database
+        Retrieves a chunk of emails from the Gmail API.
+
+        Args:
+            chunk (int): The number of emails to retrieve.
+            nextPageToken (str, optional): The token for the next page of emails. Defaults to None.
+            **parameters: Additional parameters to be passed to the API request.
+
+        Returns:
+            dict: The response from the API containing the retrieved emails.
+        """
+        return self.gmail.users().messages().list(userId='me', maxResults=chunk, pageToken=nextPageToken, **parameters).execute()
+
+    def check_emails_update(self):
+        last_local_email = self.database.get_email(-1)
+        last_email = self.get_chunk_of_emails(1,)
+        print(last_local_email, last_email)
+
+    def populate_database(self):
+        """
+            Populates the database with emails info
+            This method will always initialize a new GmailDatabase instance. If Gmail.database is not None, the database will be closed
 
             Emits:
                 emails-download-start
                 emails-download-finish
         """
-        if self.database is None:
-            self.start_database()
+        if self.database is not None:
+            print("WARNING: Overwriting Gmail.database object. Cause: Exception ocurred")
+            try:
+                self.database.close()
+            except:
+                self.database = None
+
+        self.start_database()
 
         self._add_to_prog("")
-        res = self.gmail.users().messages().list(userId='me', maxResults=50).execute()
+        res = self.get_chunk_of_emails(500)
         messages = res.get('messages')
 
-        bulk = self.gmail.new_batch_http_request()
-        for msg in messages:
-            bulk.add(self.gmail.users().messages().get(userId='me', id=msg['id']), callback=self._database_item)
-        bulk.execute()
+        total = 817
+        pos = 0
+        email_pos = 0
+        try:
+            while True:
+                print("INFO: Going back into loop... Progress: " + str(pos) + "/" + str(total))
+                for msg in messages:
+                    print(f"INFO: Parsing email {msg['id']}. Progress: {email_pos}/{500}")
+                    self.parse_email(self.gmail.users().messages().get(userId='me', id=msg['id']).execute())
+                    email_pos += 1
+
+                print('INFO: Getting next chunk...')
+                if 'nextPageToken' not in res or pos == total:
+                    break
+                else:
+                    self.database.save()
+                    res = self.get_chunk_of_emails(500, res['nextPageToken'])
+                    messages = res.get('messages')
+                    email_pos = 0
+                    pos += 500
+        except:
+            print("FATAL: Error while populating database. Saving current changes...")
+            self.database.save()
+            return
+        
+        print("INFO: Database population finished")
+    
--- src/test.py
+++ src/test.py
@@ -1,7 +1,26 @@
 from modules.gmail import Gmail
+from bs4 import BeautifulSoup
 
-def handle_finish():
-    
+import base64
+import time
+import os
+
+from datetime import datetime
+
+
+def handle_finish(gm: Gmail):
+    # gm.populate_database()
+    # last_email = list(gm.database.get_email(-1)[0])
+    # last_email[-1] = base64.b64decode(last_email[-1]).decode()
+
+    # soup = BeautifulSoup(last_email[-1], 'lxml')
+    # last_email[-1] = soup.body()
+    gm.check_emails_update()
 
 gm = Gmail()
-gm.connect('authentication-finish', )
+
+gm.start_database()
+gm.database._encrypt()
+
+gm.start()
+gm.connect('authentication-finish', handle_finish)
